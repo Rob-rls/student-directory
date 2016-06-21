5809f8db5051e31c140f0c7c945391f547f2fd09
Robert Summers <rsummers02@gmail.com>
2016-06-21 23:01:19 +0100

    ex2 Only R

--- directory.rb
+++ directory.rb
@@ -18,7 +18,11 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index {|student, i| puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"}
+  students.each_with_index do |student, i|
+    if student[:name][0] == "R"
+      puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
 end
 
 def print_footer(names)
